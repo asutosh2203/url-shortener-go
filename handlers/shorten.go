d5565843b699ba42f671edf3ddd424aed2104477
Asutosh <asutosh2001@gmail.com>
2025-05-12 17:14:51 +0530

    Added expiry time to be set by user for shortened links

--- handlers/shorten.go
+++ handlers/shorten.go
@@ -16,6 +16,7 @@ import (
 // expected request body struct
 type RequestBody struct {
 	URL string `json:"url" binding:"required"`
+	TTL int `json:"ttl"` // in hours
 }
 
 func ShortenURL(ctx *gin.Context) {
@@ -30,7 +31,13 @@ func ShortenURL(ctx *gin.Context) {
 
 	shortUrlCode := generateRandomString(5, req.URL)
 
-	if err := storage.Set(shortUrlCode, req.URL, 0); err != nil {
+	expr := time.Duration(0)
+
+	if req.TTL > 0 {
+		expr = time.Duration(req.TTL) * time.Hour
+	}
+
+	if err := storage.Set(shortUrlCode, req.URL, expr); err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to store URL"})
 		return
 	}
@@ -47,7 +54,7 @@ func generateRandomString(length int, seedStr string) string {
 		"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 	// Mix time and string seed into a single int64 seed using SHA-256
-	hash := sha256.Sum256([]byte(fmt.Sprintf("%s%d", seedStr, time.Now().UnixNano())))
+	hash := sha256.Sum256(fmt.Appendf(nil, "%s%d", seedStr, time.Now().UnixNano()))
 	seed := int64(binary.BigEndian.Uint64(hash[:8]))
 
 	// Create a new Rand instance with the combined seed
